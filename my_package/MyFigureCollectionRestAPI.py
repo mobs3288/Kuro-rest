6fc3cddcdc01a4bce5552775598fdee7246703fa
mobs3288 <pepsimanps4@gmail.com>
2024-03-19 07:11:49 +0700

    Added Cache

--- my_package/MyFigureCollectionRestAPI.py
+++ my_package/MyFigureCollectionRestAPI.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response
 import json
+from functools import lru_cache
 from my_package.figure_data import get_fig_data
 from my_package.entry_data import get_entry_data, get_entry_max_page_number
 from my_package.owned_collection_data import get_owned_figure_by_uname, get_owned_collection_max_page
@@ -20,12 +21,16 @@ class KuroRest:
     def run(self, debug=True, port=9192):
         self.app.run(debug=debug, port=port)
     
-    def get_item_data(self, item_id):
+    @staticmethod
+    @lru_cache(maxsize=128)
+    def get_item_data(item_id):
         fig_data = get_fig_data(item_id)
         fig_data_json = json.dumps(fig_data, indent=4)
         return Response(fig_data_json, mimetype='application/json')
     
-    def get_entry(self, entry_id, page_number):
+    @staticmethod
+    @lru_cache(maxsize=128)
+    def get_entry(entry_id, page_number):
         entry_data = get_entry_data(entry_id, page_number)
         max_page_number = get_entry_max_page_number(entry_id)
 
@@ -34,7 +39,9 @@ class KuroRest:
 
         return Response(entry_data_json, mimetype='application/json')
     
-    def get_owned_collection_by_uname(self, username, page_number):
+    @staticmethod
+    @lru_cache(maxsize=128)
+    def get_owned_collection_by_uname(username, page_number):
         owned_data = get_owned_figure_by_uname(username, page_number)
         max_page_number = get_owned_collection_max_page(username)
 
@@ -43,7 +50,9 @@ class KuroRest:
 
         return Response(owned_data_json, mimetype='application/json')
     
-    def get_ordered_collection_by_uname(self, username, page_number):
+    @staticmethod
+    @lru_cache(maxsize=128)
+    def get_ordered_collection_by_uname(username, page_number):
         ordered_data = get_ordered_figure_by_uname(username, page_number)
         max_page_number = get_ordered_collection_max_page(username)
 
@@ -52,7 +61,9 @@ class KuroRest:
 
         return Response(ordered_data_json, mimetype='application/json')
     
-    def get_wished_collection_by_uname(self, username, page_number):
+    @staticmethod
+    @lru_cache(maxsize=128)
+    def get_wished_collection_by_uname(username, page_number):
         wished_data = get_wished_figure_by_uname(username, page_number)
         max_page_number = get_wished_collection_max_page(username)
 
@@ -61,7 +72,9 @@ class KuroRest:
 
         return Response(wished_data_json, mimetype='application/json')
     
-    def latest_figure(self, page_number):
+    @staticmethod
+    @lru_cache(maxsize=128)
+    def latest_figure(page_number):
         latest_figure_data = get_latest_figure(page_number)
         max_page_number = get_latest_max_page()
 
